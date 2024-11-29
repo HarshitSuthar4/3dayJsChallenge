bed36d67dc25c6acf6e6bc07f6d9f56f75412d35
HarshitSuthar4 <harshitsuthar482@gmail.com>
2024-11-29 12:17:31 +0000

    data conversion and oprations

--- JS_Basics/03_dataConversion.js
+++ JS_Basics/03_dataConversion.js
@@ -1,12 +1,12 @@
-let marks = "7outOf10"
+// let marks = "7outOf10"
 
-console.log(typeof marks);
-console.log(typeof (marks));
+// console.log(typeof marks);
+// console.log(typeof (marks));
 
-let valeInNumber = Number(marks)
-console.log(typeof valeInNumber);
+// let valeInNumber = Number(marks)
+// console.log(typeof valeInNumber);
 
-console.log(valeInNumber)
+// console.log(valeInNumber)
 
 
 // "7" => 7
@@ -15,9 +15,39 @@ console.log(valeInNumber)
 // false => 0;
 
 
-let needToPass = "harshit"
+// let needToPass = "harshit"
 
-let youArePassed = Boolean(needToPass)
-console.log(youArePassed)
+// let youArePassed = Boolean(needToPass)
+// console.log(youArePassed)
 
 // harshit => true
+
+//**************** oprations **************** 
+
+let num = 2
+let negNum = -num
+// console.log(-num);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+
+// console.log("1" + 2); 
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); 
+// console.log(1 + 2 + "2"); 
+// console.log(1 + "2" + 2); 
+
+// console.log(+true);
+// console.log(+"");
+
+let scoreCounter = 100;
+scoreCounter++;
+// console.log(scoreCounter);
+
+
+
