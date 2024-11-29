c1a9faa398dce56245b835245b1e5a668e225a07
HarshitSuthar4 <harshitsuthar482@gmail.com>
2024-11-29 08:39:11 +0000

    conversion of data

---
+++ JS_Basics/03_dataConversion.js
@@ -0,0 +1,23 @@
+let marks = "7outOf10"
+
+console.log(typeof marks);
+console.log(typeof (marks));
+
+let valeInNumber = Number(marks)
+console.log(typeof valeInNumber);
+
+console.log(valeInNumber)
+
+
+// "7" => 7
+// "7outOf10" => NaN
+//  true => 1;
+// false => 0;
+
+
+let needToPass = 0
+
+let youArePassed = Boolean(needToPass)
+console.log(youArePassed)
+
+// harshit => true
