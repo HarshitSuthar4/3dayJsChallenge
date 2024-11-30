d538c8124f2e988b13fea0b8a2207da56a1e1f2e
HarshitSuthar4 <harshitsuthar482@gmail.com>
2024-11-30 14:41:12 +0000

    JS

---
+++ 01_Basics/04_dataTypesSummary.js
@@ -0,0 +1,31 @@
+/* Primitive 
+ 7 Types : String, Number, Boolean, Null, Symbol, BigInt, NaN
+*/
+
+// Examples :
+let myName = "Harshit";
+let myAge = 69;
+let isAdult = true;
+
+let temprature = null;
+const bigNumber = 565194865165748156n;
+let mySymbol = Symbol(233)
+
+/* Referance (Non primitive)
+ 3 types : [Array], {Objects}, Functions
+ */
+
+ // Examples
+ let arr = ["Harshit", "69", "Adult"];
+
+ let obj = {
+    myName: "Harshit",
+    myAge: 69
+ };
+
+ const newFnc = function (){
+    console.log("Now You know about all the data types")
+ }
+newFnc()
+ console.log( );
+ 
