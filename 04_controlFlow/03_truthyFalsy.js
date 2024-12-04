f2b16ce60e3dce6337f7d63a680cc3f55178a050
HarshitSuthar4 <harshitsuthar482@gmail.com>
2024-12-04 13:11:48 +0000

    truth, falsy and many more

--- 04_controlFlow/03_truthyFalsy.js
+++ 04_controlFlow/03_truthyFalsy.js
@@ -31,3 +31,20 @@ const obj = {}
 if (Object.keys(obj).length === 0) {
     console.log("object is empty")
 }
+
+// Nullish Coalescing Operator (??): null undefined 
+
+let val;
+val = 5 ?? 10
+val = null ?? 20
+val = undefined ?? 30
+val = null ?? 40 ?? 400
+
+// console.log(val);
+
+//Terniary Operator
+//condition ? true : false
+const iceTeaPrice = 1000
+iceTeaPrice <=900 ? console.log("Less than 900"): console.log("More than 900");
+
+
